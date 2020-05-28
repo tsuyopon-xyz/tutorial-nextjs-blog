f51544b375e73817b668a9c05c49429debdf950b
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2020-05-28 12:04:33 +0900

    Added sass.

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,9 +1,11 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
+import Layout from 'components/layout'
+
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
         <link rel="icon" href="/favicon.ico" />
@@ -14,6 +16,6 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   )
 }
