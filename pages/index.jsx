66f5920d8a96ca57ae67d7efeb9befa5cd6a0ff0
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2020-05-28 14:01:31 +0900

    Done Dynamic Routes part in the tutorial.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,27 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import Layout, { siteTitle } from 'components/layout'
-import utilStyles from 'styles/utils.module.scss'
-import { getSortedPostsData } from 'lib/posts'
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout, { siteTitle } from 'components/layout';
+import Date from 'components/date';
+import utilStyles from 'styles/utils.module.scss';
+import { getSortedPostsData } from 'lib/posts';
 
-export default function Home({allPostsData}) {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>NextのTutorialに取り組んでます。(<a href="https://nextjs.org/learn/basics/create-nextjs-app" target="_blank">https://nextjs.org/learn/basics/create-nextjs-app</a>)</p>
+        <p>
+          NextのTutorialに取り組んでます。(
+          <a
+            href="https://nextjs.org/learn/basics/create-nextjs-app"
+            target="_blank"
+          >
+            https://nextjs.org/learn/basics/create-nextjs-app
+          </a>
+          )
+        </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
@@ -22,25 +32,27 @@ export default function Home({allPostsData}) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              <Link href="/posts/[id]" as={`/posts/${id}`} ><a>{title}</a></Link>
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
       </section>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+  const allPostsData = getSortedPostsData();
 
   return {
     props: {
-      allPostsData
-    }
-  }
+      allPostsData,
+    },
+  };
 }
